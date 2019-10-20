021035fad3ba9412084dd97a9bfe955211d8be5d
JILOLO <jiyuliang526@gmail.com>
2019-10-19 19:41:37 -0800

    update

--- createdb.py
+++ createdb.py
@@ -1,10 +1,12 @@
+# Inserting into a Database with SQLite
+
 import sqlite3
 
 conn = sqlite3.connect('tutorial.db')
 c = conn.cursor
 
 def create_table():
-    c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEST, keyword TEXT, value REAL)")
+    c.execute("CREATE TABLE IF NOT EXISTS myTable(unix REAL, datestamp TEST, keyword TEXT, value REAL)")
 
 def data_entry():
     c.execute("INSERT INTO stuffToPlot VALUES(881101,'2019-10-19','python',6)")
